4c3b7d707fa4c174d2757e252525f32149e92655
Daniil Matveev <ihatemat@yandex.ru>
2020-04-20 14:09:31 +0500

    забыл на мобильной версии про слайдер сделал

--- source/js/app.js
+++ source/js/app.js
@@ -31,6 +31,20 @@ document.querySelector(".main-nav__toggle").addEventListener("click",function(){
   button.classList.toggle("main-nav__toggle--close")
   nav.classList.toggle("nav-list--active")
 })
+var before=document.querySelector(".progress__result--before")
+var after = document.querySelector(".progress__result--after")
+var thumb= document.querySelector(".progress__slider-thumb")
+var progress__btn=document.querySelectorAll(".progress__slider-button")
+progress__btn[0].addEventListener("click",function(){
+  before.style.opacity="1"
+  after.style.opacity="0"
+  thumb.style.left="6px"
+})
+progress__btn[1].addEventListener("click", function () {
+  before.style.opacity = "0";
+  after.style.opacity = "1";
+  thumb.style.left = "42px";
+});
 document.querySelector(".progress__input").addEventListener("input",function(){
   var value=this.value;
   document.querySelector(".progress__result--before").style.width = value + "%";
